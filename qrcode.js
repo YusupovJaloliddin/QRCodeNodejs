c12f63514ddfbaf0a7160f7a73c5f1498f00751c
YusupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2022-10-06 15:54:23 +0500

    updated qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -1,17 +1,11 @@
-// const qrNode = require("qrcode");
+const qrNode = require("qrcode");
 
-// const generator = async (text) => {
-//   try {
-//     console.log(await qrNode.toDataURL(text));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// generator("salom");
-var QRCode = require("qrcode");
-var canvas = document.getElementById("canvas");
+const generator = async (text) => {
+  try {
+    console.log(await qrNode.toDataURL(text));
+  } catch (error) {
+    console.log(error);
+  }
+};
+generator("salom");
 
-QRCode.toCanvas(canvas, "sample text", function (error) {
-  if (error) console.error(error);
-  console.log("success!");
-});
